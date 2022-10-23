4fce887a1b5f87a5a7713762c4028ed1e8c868e3
LuisBels <luisbelr9@gmail.com>
2022-10-23 18:11:59 -0400

    Creamos el template de index y pusimos las urls

--- premiosapp/polls/views.py
+++ premiosapp/polls/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-  return HttpResponse("Hello World")
+  last_question_list = Question.objects.all()
+  return render(request,"polls/index.html", {
+    "last_question_list": last_question_list
+  })
+
+def detail(request, question_id):
+  return HttpResponse(f"Estas viendo las opciones de la pregunta numero{question_id}")
+
+
+def results(request, question_id):
+  return HttpResponse(f"Estas viendo las opciones de la pregunta numero{question_id}")
