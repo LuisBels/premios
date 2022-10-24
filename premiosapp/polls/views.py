5b829acd31395084c698b5f4bc6f3f2146b31fd4
LuisBels <luisbelr9@gmail.com>
2022-10-23 23:25:00 -0400

    Creamos el template de detail y las generic views

--- premiosapp/polls/views.py
+++ premiosapp/polls/views.py
@@ -1,16 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
+from django.views import generic
+from django.utils import timezone
 
-def index(request):
-  last_question_list = Question.objects.all()
-  return render(request,"polls/index.html", {
-    "last_question_list": last_question_list
-  })
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = "last_question_list"
 
-def detail(request, question_id):
-  return HttpResponse(f"Estas viendo las opciones de la pregunta numero{question_id}")
+    def get_queryset(self): 
+        """Return the last five published questions (not including those set to be published in the future)."""
+        # return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
 
-def results(request, question_id):
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView): 
+    model = Question
+    template_name = 'polls/results.html'
+
+def vote(request, question_id):
   return HttpResponse(f"Estas viendo las opciones de la pregunta numero{question_id}")
