dbf87e2f5879225af14f62e183bc9f5d92076930
LuisBels <luisbelr9@gmail.com>
2022-10-24 17:58:48 -0400

    Creamos la view de vote

--- premiosapp/polls/views.py
+++ premiosapp/polls/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Question
+from django.urls import reverse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
 from django.views import generic
 from django.utils import timezone
 
@@ -30,4 +31,16 @@ class ResultsView(generic.DetailView):
     template_name = 'polls/results.html'
 
 def vote(request, question_id):
-  return HttpResponse(f"Estas viendo las opciones de la pregunta numero{question_id}")
+  question = get_object_or_404(Question, pk=question_id)
+  try:
+    select_choice = question.choice_set.get(pk=request.POST["choice"])
+  except (KeyError, Choice.DoesNotExist):
+    return render(request, "polls/detail.html", {
+      "question": question,
+      "error_message": "You didn't select a choice"
+    })
+
+  else:
+    select_choice.votes += 1
+    select_choice.save()
+    return HttpResponseRedirect(reverse("polls:results", args=(question.id)))
